13e7d0f2e64eee6694227fc21f8a999fb726c5c9
Bazyli Cyran <bazyli.cyran@gmail.com>
2022-11-03 19:34:43 +0100

    feat: Add Version type and parsing

--- version/version.go
+++ version/version.go
@@ -1,5 +1,34 @@
 package version
 
+import (
+	"regexp"
+	"strings"
+)
+
+type Version string
+
+var versionRegexp = regexp.MustCompile(`^[\w.]+$`)
+
+func parseVersion(rawVersion string) (Version, bool) {
+	if !containsDigit(rawVersion) {
+		return Version(""), false
+	}
+	if !versionRegexp.MatchString(rawVersion) {
+		return Version(""), false
+	}
+	rawVersion = strings.TrimPrefix(rawVersion, "v")
+	return Version(rawVersion), true
+}
+
+func containsDigit(str string) bool {
+	for _, char := range str {
+		if char >= '0' && char <= '9' {
+			return true
+		}
+	}
+	return false
+}
+
 type VersionProvider interface {
 	LatestVersion() (string, error)
 }
---
+++ version/version_test.go
@@ -0,0 +1,39 @@
+package version
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseVersion_Valid(t *testing.T) {
+	cases := map[string]Version{
+		"v1.2.3": Version("1.2.3"),
+		"1.2.3":  Version("1.2.3"),
+		"1.2_b3": Version("1.2_b3"),
+		"10":     Version("10"),
+	}
+
+	for rawVersion, expectedResult := range cases {
+		result, valid := parseVersion(rawVersion)
+
+		assert.True(t, valid)
+		assert.Equal(t, expectedResult, result)
+	}
+}
+
+func TestParseVersion_Invalid(t *testing.T) {
+	cases := []string{
+		"abc",
+		"",
+		"1.2-b3",
+		"1.2.%3",
+		"1:2:3",
+	}
+
+	for _, rawVersion := range cases {
+		_, valid := parseVersion(rawVersion)
+
+		assert.False(t, valid)
+	}
+}
