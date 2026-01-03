1d814681f976b7882c3efe904b9be9ff2923c96c
Bazyli Cyran <bazyli@cyran.dev>
2026-01-03 17:18:15 +0100

    chore: Bump version to 1.0.2

--- cmd/bumper/bumper.go
+++ cmd/bumper/bumper.go
@@ -59,7 +59,7 @@ enables you to run bumper in a dir containing multiple package dirs.`,
   bumper --bump=false                   find packages, check updates in $PWD
   bumper ~/workspace/aur                find and bump packages in given dir
   bumper ~/workspace/aur/my-package     bump single package`,
-	Version: "1.0.1",
+	Version: "1.0.2",
 	Run: func(cmd *cobra.Command, args []string) {
 		if debug {
 			bumper.EnableDebugLogging()
